885c7cc32d0ee6a45d1bf72a96229a01e5325044
Adrian Michalek <devmichalek@gmail.com>
2024-07-03 06:31:02 +0200

    Reduce number of scenarios

--- engine/autotests/tteams-engine-autotests-dummy-neighbor.py
+++ engine/autotests/tteams-engine-autotests-dummy-neighbor.py
@@ -13,29 +13,21 @@
 # Scenarios
 # 1.1 Always reply with HeartbeatReply on HeartbeatRequest
 # 1.2 Reply with HeartbeatReply n times on HeartbeatReqest then stop replying
-# 1.3 Never reply with HeartbeatReply on HeartbeatRequest
 # 2.1 Request with HeartbeatRequest every t seconds
-# 2.2 Never request with HeartbeatRequest
 # 3.1 Always reply with GreetReply on GreetRequest
-# 3.2 Never reply with GreetReply on GreetRequest
 # 4.1 Request with GreetRequest n times every t seconds until first GreetReply
-# 4.2 Never request with GreetRequest
 # 5.1 Always reply with TellReply on TellRequest
-# 5.2 Reply with TellReply every n times for particular TellRequest sequence number
-# 5.3 Never reply with TellReply on TellRequest
-# 6.1 Request with TellRequest every n received message
-# 6.2 Request with TellRequest every t seconds, up to n messages
-# 6.3 Never request with TellRequest
+# 5.2 Reply with TellReply every second time for particular TellRequest sequence number
+# 6.1 Request with TellRequest every t seconds, up to n messages
 # 7.1 Always reply with NarrateReply on NarrateReply
-# 7.2 Reply with NarratReply every second time on NarrateReply
-# 7.3 Never reply with NarrateReply on NarrateReply
+# 7.2 Reply with NarratReply every second time for particular NarrateReply sequence number
 # 8.1 Request with NarrateRequest once
-# 8.2 Never request with NarrateRequest
 
 import argparse
 import logging
 import grpc
 import concurrent
+import time
 import signal
 import threading
 import TerminalTeams_pb2
@@ -50,7 +42,8 @@ def scenario_1_1(src_ip_address, src_port, dst_ip_address, dst_port):
         server_shutdown_event.set()
     class Servicer(TerminalTeams_pb2_grpc.NeighborsDiscovery):
         def Heartbeat(self, request, context):
-            return TerminalTeams_pb2.HeartbeatReply(identity="dummy")
+            logging.info("Sending dummy HeartbeatReply...")
+            return TerminalTeams_pb2.HeartbeatReply(identity="identity")
     signal.signal(signal.SIGTERM, stop_server)
     server = grpc.server(concurrent.futures.ThreadPoolExecutor(max_workers=1))
     TerminalTeams_pb2_grpc.add_NeighborsDiscoveryServicer_to_server(Servicer(), server)
@@ -74,7 +67,8 @@ def scenario_1_2(src_ip_address, src_port, dst_ip_address, dst_port):
     class Servicer(TerminalTeams_pb2_grpc.NeighborsDiscovery):
         def Heartbeat(self, request, context):
             count_heartbeat()
-            return TerminalTeams_pb2.HeartbeatReply(identity="dummy")
+            logging.info("Sending dummy HeartbeatReply...")
+            return TerminalTeams_pb2.HeartbeatReply(identity="identity")
     signal.signal(signal.SIGTERM, stop_server)
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
     TerminalTeams_pb2_grpc.add_NeighborsDiscoveryServicer_to_server(Servicer(), server)
@@ -84,85 +78,205 @@ def scenario_1_2(src_ip_address, src_port, dst_ip_address, dst_port):
     server_shutdown_event.wait()
     server.stop(3).wait()
 
-def scenario_1_3(src_ip_address, src_port, dst_ip_address, dst_port):
-    logging.info("Running scenario 1.3...")
-
 def scenario_2_1(src_ip_address, src_port, dst_ip_address, dst_port):
     logging.info("Running scenario 2.1...")
-
-def scenario_2_2(src_ip_address, src_port, dst_ip_address, dst_port):
-    logging.info("Running scenario 2.2...")
+    client_shutdown_event = threading.Event()
+    def stop_client(signum, frame):
+        server_shutdown_event.set()
+    signal.signal(signal.SIGTERM, stop_client)
+    with grpc.insecure_channel(dst_ip_address + ":" + dst_port) as channel:
+        stub = TerminalTeams_pb2_grpc.NeighborsDiscoveryStub(channel)
+        while not client_shutdown_event.is_set():
+            try:
+                logging.info("Sending dummy HeartbeatRequest...")
+                stub.Heartbeat(TerminalTeams_pb2.HeartbeatRequest(identity="identity"))
+            except Exception:
+                logging.error("Failed to send dummy HeartbeatRequest!")
+                pass
+            time.sleep(2)
+    client_shutdown_event.wait()
 
 def scenario_3_1(src_ip_address, src_port, dst_ip_address, dst_port):
     logging.info("Running scenario 3.1...")
-
-def scenario_3_2(src_ip_address, src_port, dst_ip_address, dst_port):
-    logging.info("Running scenario 3.2...")
+    server_shutdown_event = threading.Event()
+    def stop_server(signum, frame):
+        server_shutdown_event.set()
+    class Servicer(TerminalTeams_pb2_grpc.NeighborsDiscovery):
+        def Heartbeat(self, request, context):
+            logging.info("Sending dummy GreetReply...")
+            return TerminalTeams_pb2.GreetReply(nickname="nickname", identity="identity", ipAddressAndPort=src_ip_address + ":" + src_port)
+    signal.signal(signal.SIGTERM, stop_server)
+    server = grpc.server(concurrent.futures.ThreadPoolExecutor(max_workers=1))
+    TerminalTeams_pb2_grpc.add_NeighborsDiscoveryServicer_to_server(Servicer(), server)
+    server.add_insecure_port(src_ip_address + ":" + src_port)
+    server.start()
+    logging.info("Server started, listening on " + src_ip_address + ":" + src_port)
+    server_shutdown_event.wait()
+    server.stop(3).wait()
 
 def scenario_4_1(src_ip_address, src_port, dst_ip_address, dst_port):
     logging.info("Running scenario 4.1...")
-
-def scenario_4_2(src_ip_address, src_port, dst_ip_address, dst_port):
-    logging.info("Running scenario 4.2...")
+    client_shutdown_event = threading.Event()
+    greet_counter = 0
+    greet_limit = 5
+    def stop_client(signum, frame):
+        server_shutdown_event.set()
+    def count_greet():
+        greet_counter += 1
+        if greet_counter >= greet_limit:
+            stop_client(0, 0)
+    signal.signal(signal.SIGTERM, stop_client)
+    with grpc.insecure_channel(dst_ip_address + ":" + dst_port) as channel:
+        stub = TerminalTeams_pb2_grpc.NeighborsDiscoveryStub(channel)
+        while not client_shutdown_event.is_set():
+            try:
+                logging.info("Sending dummy GreetRequest...")
+                stub.Heartbeat(TerminalTeams_pb2.GreetRequest(nickname="nickname", identity="identity", ipAddressAndPort=src_ip_address + ":" + src_port))
+            except Exception:
+                logging.error("Failed to send dummy GreetRequest!")
+                pass
+            count_greet()
+            time.sleep(2)
+    client_shutdown_event.wait()
 
 def scenario_5_1(src_ip_address, src_port, dst_ip_address, dst_port):
     logging.info("Running scenario 5.1...")
+    server_shutdown_event = threading.Event()
+    def stop_server(signum, frame):
+        server_shutdown_event.set()
+    class Servicer(TerminalTeams_pb2_grpc.NeighborsChat):
+        def Tell(self, request, context):
+            logging.info("Sending dummy TellReply...")
+            return TerminalTeams_pb2.TellReply(identity="identity")
+    signal.signal(signal.SIGTERM, stop_server)
+    server = grpc.server(concurrent.futures.ThreadPoolExecutor(max_workers=1))
+    TerminalTeams_pb2_grpc.add_NeighborsDiscoveryServicer_to_server(Servicer(), server)
+    server.add_insecure_port(src_ip_address + ":" + src_port)
+    server.start()
+    logging.info("Server started, listening on " + src_ip_address + ":" + src_port)
+    server_shutdown_event.wait()
+    server.stop(3).wait()
 
 def scenario_5_2(src_ip_address, src_port, dst_ip_address, dst_port):
     logging.info("Running scenario 5.2...")
-
-def scenario_5_3(src_ip_address, src_port, dst_ip_address, dst_port):
-    logging.info("Running scenario 5.3...")
+    server_shutdown_event = threading.Event()
+    def stop_server(signum, frame):
+        server_shutdown_event.set()
+    sequence_numbers = {}
+    class Servicer(TerminalTeams_pb2_grpc.NeighborsChat):
+        def Tell(self, request, context):
+            logging.info("Sending dummy TellReply...")
+            if request.sequenceNumber in sequence_numbers:
+                return TerminalTeams_pb2.TellReply(identity="identity")
+            sequence_numbers.add(request.sequenceNumber)
+            return TerminalTeams_pb2.TellReply(identity="error")
+    signal.signal(signal.SIGTERM, stop_server)
+    server = grpc.server(concurrent.futures.ThreadPoolExecutor(max_workers=1))
+    TerminalTeams_pb2_grpc.add_NeighborsDiscoveryServicer_to_server(Servicer(), server)
+    server.add_insecure_port(src_ip_address + ":" + src_port)
+    server.start()
+    logging.info("Server started, listening on " + src_ip_address + ":" + src_port)
+    server_shutdown_event.wait()
+    server.stop(3).wait()
 
 def scenario_6_1(src_ip_address, src_port, dst_ip_address, dst_port):
     logging.info("Running scenario 6.1...")
-
-def scenario_6_2(src_ip_address, src_port, dst_ip_address, dst_port):
-    logging.info("Running scenario 6.2...")
-
-def scenario_6_3(src_ip_address, src_port, dst_ip_address, dst_port):
-    logging.info("Running scenario 6.3...")
+    client_shutdown_event = threading.Event()
+    tell_counter = 0
+    tell_limit = 5
+    def stop_client(signum, frame):
+        server_shutdown_event.set()
+    def count_tell():
+        tell_counter += 1
+        if tell_counter >= tell_limit:
+            stop_client(0, 0)
+    signal.signal(signal.SIGTERM, stop_client)
+    with grpc.insecure_channel(dst_ip_address + ":" + dst_port) as channel:
+        stub = TerminalTeams_pb2_grpc.NeighborsChatStub(channel)
+        while not client_shutdown_event.is_set():
+            try:
+                logging.info("Sending dummy TellRequest...")
+                stub.Tell(TerminalTeams_pb2.TellRequest(identity="identity", message=("msg" + str(tell_counter)), sequenceNumber=tell_counter, timestamp=""))
+            except Exception:
+                logging.error("Failed to send dummy TellRequest!")
+                pass
+            count_tell()
+            time.sleep(2)
+    client_shutdown_event.wait()
 
 def scenario_7_1(src_ip_address, src_port, dst_ip_address, dst_port):
     logging.info("Running scenario 7.1...")
+    server_shutdown_event = threading.Event()
+    def stop_server(signum, frame):
+        server_shutdown_event.set()
+    class Servicer(TerminalTeams_pb2_grpc.NeighborsChat):
+        def Narrate(self, request_iterator, context):
+            logging.info("Sending dummy NarrateReply...")
+            return TerminalTeams_pb2.NarrateReply(identity="identity")
+    signal.signal(signal.SIGTERM, stop_server)
+    server = grpc.server(concurrent.futures.ThreadPoolExecutor(max_workers=1))
+    TerminalTeams_pb2_grpc.add_NeighborsDiscoveryServicer_to_server(Servicer(), server)
+    server.add_insecure_port(src_ip_address + ":" + src_port)
+    server.start()
+    logging.info("Server started, listening on " + src_ip_address + ":" + src_port)
+    server_shutdown_event.wait()
+    server.stop(3).wait()
 
 def scenario_7_2(src_ip_address, src_port, dst_ip_address, dst_port):
     logging.info("Running scenario 7.2...")
+    server_shutdown_event = threading.Event()
+    def stop_server(signum, frame):
+        server_shutdown_event.set()
+    sequence_numbers = {}
+    class Servicer(TerminalTeams_pb2_grpc.NeighborsChat):
+        def Narrate(self, request_iterator, context):
+            logging.info("Sending dummy NarrateReply...")
+            if request.sequenceNumber in sequence_numbers:
+                return TerminalTeams_pb2.NarrateReply(identity="identity")
+            sequence_numbers.add(request.sequenceNumber)
+            return TerminalTeams_pb2.NarrateReply(identity="error")
+    signal.signal(signal.SIGTERM, stop_server)
+    server = grpc.server(concurrent.futures.ThreadPoolExecutor(max_workers=1))
+    TerminalTeams_pb2_grpc.add_NeighborsDiscoveryServicer_to_server(Servicer(), server)
+    server.add_insecure_port(src_ip_address + ":" + src_port)
+    server.start()
+    logging.info("Server started, listening on " + src_ip_address + ":" + src_port)
+    server_shutdown_event.wait()
+    server.stop(3).wait()
 
-def scenario_7_3(src_ip_address, src_port, dst_ip_address, dst_port):
-    logging.info("Running scenario 7.3...")
-
-def scenario_8_1(src_ip_address, src_port, dst_ip_address, dst_port):
-    logging.info("Running scenario 8.1...")
-
-def scenario_8_2(src_ip_address, src_port, dst_ip_address, dst_port):
-    logging.info("Running scenario 8.2...")
+# def scenario_8_1(src_ip_address, src_port, dst_ip_address, dst_port):
+#     logging.info("Running scenario 8.1...")
+#     client_shutdown_event = threading.Event()
+#     def stop_client(signum, frame):
+#         server_shutdown_event.set()
+#     signal.signal(signal.SIGTERM, stop_client)
+#     with grpc.insecure_channel(dst_ip_address + ":" + dst_port) as channel:
+#         stub = TerminalTeams_pb2_grpc.NeighborsChatStub(channel)
+#         try:
+#             logging.info("Sending dummy NarrateRequest...")
+#             stub.Narrate(TerminalTeams_pb2.NarrateRequest(identity="identity", message=("msg" + str(narrate_counter)), sequenceNumber=tell_counter, timestamp=""))
+#         except Exception:
+#             logging.error("Failed to send dummy NarrateRequest!")
+#             pass
+#         time.sleep(2)
+#     client_shutdown_event.wait()
 
 scenarios = {
     '1.1': scenario_1_1,
     '1.2': scenario_1_2,
-    '1.3': scenario_1_3,
     '2.1': scenario_2_1,
-    '2.2': scenario_2_2,
     '3.1': scenario_3_1,
-    '3.2': scenario_3_2,
     '4.1': scenario_4_1,
-    '4.2': scenario_4_2,
     '5.1': scenario_5_1,
     '5.2': scenario_5_2,
-    '5.3': scenario_5_3,
     '6.1': scenario_6_1,
-    '6.2': scenario_6_2,
-    '6.3': scenario_6_3,
     '7.1': scenario_7_1,
     '7.2': scenario_7_2,
-    '7.3': scenario_7_3,
-    '8.1': scenario_8_1,
-    '8.2': scenario_8_2
+    '8.1': scenario_8_1
 }
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
     parser = argparse.ArgumentParser()
     parser.add_argument("--src-ip-address", dest="src_ip_address", help="Source (host) IP address", required=True)
     parser.add_argument("--dst-ip-address", dest="dst_ip_address", help="Destination (client) IP address", required=True)
